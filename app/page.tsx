28e63dd8bfa2418128d7a50019bd219c39a45629
Ilya <honeyflym@gmail.com>
2024-08-18 21:25:24 +0300

    build project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <h1>Home
+      <Button variant="outline">Button</Button>
+   </h1>
+ 
+  );
+}
