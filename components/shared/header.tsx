c3a9cde5e6a0390bec59d06e3a903527f52a71d3
Ilya <honeyflym@gmail.com>
2024-08-21 21:15:51 +0300

    add container

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { cn } from "@/lib/utils"
+import { Container } from "./container";
+import Image from "next/image";
+
 
 interface Props {
     className?: string;
@@ -6,6 +10,17 @@ interface Props {
 
 export const Header: React.FC<Props> = ({ className }) => {
 return (
-    <div className={className}></div>
+    <header className={cn('border border-b', className)}>
+       <Container className="flex items-center justify-between py-8">
+        <div>
+            <Image src="/logo.png" alt="logo" width={35} height={35} />
+            <div>
+                <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                <p className="text-sm text-gray-400 leading-3">The best pizza in town</p>
+            </div>
+        </div>
+     
+       </Container>
+    </header>
 )
 }
