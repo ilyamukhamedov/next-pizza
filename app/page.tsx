44e06a364516349d1934e5e1840169a34f60b96d
Ilya <honeyflym@gmail.com>
2024-09-14 16:57:05 +0300

    {

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Container, Filters, Title, TopBar } from "@/components/shared";
 import { ProductCard } from "@/components/shared/product-card";
+import { ProductsGroupList } from "@/components/shared/products-group-list";
 
 export default function Home() {
   return (
@@ -11,12 +12,27 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
           <div className="flex-1">
-            <div className="flex flex-col gap-16"></div>
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Pizza"
+                categoryId={1}
+                items={[
+                  {
+                    id: 1,
+                    name: "Hawaiian",
+                    price: 10,
+                    imageUrl:
+                      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+                    items: [{ price: 5.5 }],
+                  },
+                ]}
+              />
+            </div>
           </div>
         </div>
       </Container>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,23 +1,42 @@
-import { cn } from '@/lib/utils';
-import React from 'react';
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useCategoryStore } from "@/store/category";
+import React from "react";
 
 interface Props {
-    className?: string;
+  className?: string;
 }
 
-const cats = ['Pizza', 'Sets', 'Starters', 'Cocktails', 'Coffee', 'Drinks', 'Deserts'];
-const activeIndex = 0;
+const cats = [
+  { id: 0, name: "Pizza" },
+  { id: 1, name: "Sets" },
+  { id: 2, name: "Starters" },
+  { id: 3, name: "Cocktails" },
+  { id: 4, name: "Coffee" },
+  { id: 5, name: "Drinks" },
+  { id: 6, name: "Deserts" },
+];
 
 export const Categories: React.FC<Props> = ({ className }) => {
-return (
-    <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl ', className)}>
-        {cats.map((cat, index) => (
-            <a className={cn(
-                'flex items-center font-bold h-11 rounded-2xl px-5', 
-                activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary'
-            )} key={index}>
-                <button>{cat}</button>
-            </a>
-        ))}
+  const activeCategoryId = useCategoryStore((state) => state.activeId);
+  return (
+    <div
+      className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl ", className)}
+    >
+      {cats.map(({ name, id }, index) => (
+        <a
+          className={cn(
+            "flex items-center font-bold h-11 rounded-2xl px-5",
+            activeCategoryId === id &&
+              "bg-white shadow-md shadow-gray-200 text-primary"
+          )}
+          href={`/#${name}`}
+          key={index}
+        >
+          <button>{name}</button>
+        </a>
+      ))}
     </div>
-) };
+  );
+};
